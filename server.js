7a9cea2307b61970fb4e8db420d315632d7f9345
HenryP23 <69431847+HenryP23@users.noreply.github.com>
2020-10-27 18:54:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const mongoose = require ('mongoose')
 const app = express();
-const routes = require('./routes');
-const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const routes = require('./routes')
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,13 +17,12 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 app.use(routes);
-// Send every other request to the React app
-// Define any API routes before this runs
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/searchDb");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/searchDb", {useNewUrlParser: true});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
